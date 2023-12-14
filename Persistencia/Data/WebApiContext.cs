50a36f8222791e8e5d7f26677079544e77ebe35f
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-12-14 14:05:41 -0500

    feat: :sparkles: Added File Context
    @Gonther1

---
+++ Persistencia/Data/WebApiContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistencia.Data;
+
+public partial class WebApiContext : DbContext
+{
+    public WebApiContext()
+    {
+    }
+
+    public WebApiContext(DbContextOptions<WebApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categorium> Categoria { get; set; }
+
+    public virtual DbSet<Ciudad> Ciudads { get; set; }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Color> Colors { get; set; }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Detallepedido> Detallepedidos { get; set; }
+
+    public virtual DbSet<Direccion> Direccions { get; set; }
+
+    public virtual DbSet<Estadopedido> Estadopedidos { get; set; }
+
+    public virtual DbSet<Inventario> Inventarios { get; set; }
+
+    public virtual DbSet<Pai> Pais { get; set; }
+
+    public virtual DbSet<Pedido> Pedidos { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Tipomaterial> Tipomaterials { get; set; }
+
+    public virtual DbSet<Tipopago> Tipopagos { get; set; }
+
+    public virtual DbSet<Tipoproducto> Tipoproductos { get; set; }
+
+    public virtual DbSet<Transaccione> Transacciones { get; set; }
+
+    /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=campus2024;database=nikedata", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+ */
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
