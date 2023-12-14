b3a1263ab64518d7b389d892100c1b1d7cad013b
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-12-14 14:06:06 -0500

    feat: :sparkles: Added File with extensions
    @Gonther1

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aplicacion.UnitOfWork;
+using AspNetCoreRateLimit;
+using Dominio.Interfaces;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options => {
+            options.AddPolicy("CorsPolicy", builder=>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+
+        public static void AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork,UnitOfWork>();
+        }
+        public static void ConfigureRateLimiting(this IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddInMemoryRateLimiting();
+            services.Configure<IpRateLimitOptions>(options=>{
+                options.EnableEndpointRateLimiting = true;
+                options.StackBlockedRequests = false;
+                options.HttpStatusCode = 429;
+                options.RealIpHeader = "X-Real-IP";
+                options.GeneralRules = new List<RateLimitRule>
+                {
+                    new RateLimitRule
+                    {
+                        Endpoint = "*",
+                        Period = "10s",
+                        Limit = 2
+                    }
+                };
+            });
+        }
+    }
+}
